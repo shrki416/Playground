43b4b1affc942549ca121237e3f48793eef982a0
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2020-12-26 17:42:01 -0500

    use .env variables

--- chat-app/chat-backend/server.js
+++ chat-app/chat-backend/server.js
@@ -1,7 +1,8 @@
 const express = require("express");
+const config = require("./config/app");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = config.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send("Hello World");
