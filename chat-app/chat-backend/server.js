14e731123172504a2a5d8e53daa4d5eda0908617
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2020-12-26 18:10:47 -0500

    add login route

--- chat-app/chat-backend/server.js
+++ chat-app/chat-backend/server.js
@@ -4,8 +4,12 @@ require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
+app.get("/home", (req, res) => {
   res.send("Hello World");
+});
+
+app.get("/login", (res, req) => {
+  res.send("Hello from login page");
 });
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
