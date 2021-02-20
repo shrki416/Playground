192beac0e303313e90a803f5b148d01d58cd8bb8
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2021-02-20 10:48:01 -0500

    Start basic routing and install react-router-dom

--- chat-app/chat-frontend/src/App.js
+++ chat-app/chat-frontend/src/App.js
@@ -1,7 +1,23 @@
 import "./App.css";
+import Login from "./components/Auth/Login";
+import Register from "./components/Auth/Register";
+import Chat from "./components/Chat/Chat";
+
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
-  return <div className="App">Chat App</div>;
+  return (
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path="/" component={Chat} />
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
+          <Route render={() => <h1>404: Page Not Found</h1>} />
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
