4a905a30df26dd1ca591ad13467bc92a54661945
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2021-02-19 20:26:51 -0500

    generate random password tokens

--- chat-app/chat-backend/controller/authController.js
+++ chat-app/chat-backend/controller/authController.js
@@ -1,5 +1,6 @@
 const User = require("../models").User;
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 exports.login = async (req, res) => {
   const { email, password } = req.body;
@@ -7,16 +8,28 @@ exports.login = async (req, res) => {
   try {
     // find the user with this email address
     const user = await User.findOne({ where: { email } });
+
     // check if the user is found
     if (!user) return res.status(404).json({ message: "User does not exist" });
+
     // check if password matches
     if (!bcrypt.compareSync(password, user.password))
       return res.status(401).json({ message: "incorrect password " });
+
     // generate auth token
-    return res.send(user);
-  } catch (error) {}
+    const userWithToken = generateToken(user.get({ raw: true }));
+    return res.send(userWithToken);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
 
   return res.send([email, password]);
 };
 
 exports.register = async (req, res) => {};
+
+const generateToken = (user) => {
+  delete user.password;
+  const token = jwt.sign(user, "secret", { expiresIn: 86400 });
+  return { ...user, ...{ token } };
+};
