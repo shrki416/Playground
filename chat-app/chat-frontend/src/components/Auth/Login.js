e4dcd801bc1f86c1c5a54e46f7666084a0dbccd9
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2021-02-21 11:16:34 -0500

    login user

--- chat-app/chat-frontend/src/components/Auth/Login.js
+++ chat-app/chat-frontend/src/components/Auth/Login.js
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import login from "../../assests/images/login.svg";
 import { Link } from "react-router-dom";
+// import axios from "axios";
+import AuthService from "../../services/authService";
 
 import "./Auth.scss";
 
 function Login() {
+  const [email, setEmail] = useState("john.doe@email.com");
+  const [password, setPassword] = useState("secrets");
+
+  const submitForm = (e) => {
+    e.preventDefault();
+
+    AuthService.login({ email, password }).then((res) => console.log(res));
+    // axios
+    //   .post("http://localhost:3000/login", { email, password })
+    //   .then((res) => console.log("res", res))
+    //   .catch((err) => console.log("err", err));
+
+    console.log({ email, password });
+    setEmail("");
+    setPassword("");
+  };
+
   return (
     <div id="auth-container">
       <div id="auth-card">
@@ -14,12 +33,22 @@ function Login() {
           </div>
           <div id="form-section">
             <h2>Welcome Back</h2>
-            <form>
+            <form onSubmit={submitForm}>
               <div className="input-field mb-1">
-                <input placeholder="Email" />
+                <input
+                  placeholder="Email"
+                  value={email}
+                  type="email"
+                  onChange={(e) => setEmail(e.target.value)}
+                />
               </div>
               <div className="input-field mb-2">
-                <input placeholder="Password" />
+                <input
+                  placeholder="Password"
+                  type="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
               </div>
               <button>LOGIN</button>
               <p>
