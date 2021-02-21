5d7def88775972f1cbb3d686f406ab73cc10757b
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2021-02-21 11:15:11 -0500

    add cors

--- chat-app/chat-backend/server.js
+++ chat-app/chat-backend/server.js
@@ -5,9 +5,10 @@ const config = require("./config/app");
 const router = require("./router");
 
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
-
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
