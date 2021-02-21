68600d5f88c2ffc3891b4353d8cef97c63c28b5f
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2021-02-21 09:39:44 -0500

    finish styling login component

--- chat-app/chat-frontend/src/components/Auth/Login.js
+++ chat-app/chat-frontend/src/components/Auth/Login.js
@@ -7,17 +7,17 @@ function Login() {
   return (
     <div id="auth-container">
       <div id="auth-card">
-        <div>
+        <div className="card-shadow">
           <div id="image-section">
             <img src={login} alt="login" />
           </div>
           <div id="form-section">
             <h2>Welcome Back</h2>
             <form>
-              <div className="input-field">
+              <div className="input-field mb-1">
                 <input placeholder="Email" />
               </div>
-              <div className="input-field">
+              <div className="input-field mb-2">
                 <input placeholder="Password" />
               </div>
               <button>LOGIN</button>
