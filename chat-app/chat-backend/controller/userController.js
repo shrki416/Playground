fa8ff434bcce205dfee95949af689b94d7b38c6e
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2021-02-23 16:01:56 -0500

    prepare BE for image upload

--- chat-app/chat-backend/controller/userController.js
+++ chat-app/chat-backend/controller/userController.js
@@ -2,6 +2,11 @@ const User = require("../models").User;
 const sequelize = require("sequelize");
 
 exports.update = async (req, res) => {
+  if (req.file) req.body.avatar = req.file.filename;
+
+  if (typeof req.body.avatar !== "undefined" && req.body.avatar.length === 0)
+    delete req.body.avatar;
+
   try {
     const [rows, result] = await User.update(req.body, {
       where: {
@@ -11,6 +16,8 @@ exports.update = async (req, res) => {
       individualHooks: true,
     });
     const user = result[0].get({ row: true });
+    console.log(user);
+
     user.avatar = result[0].avatar;
     delete user.password;
 
--- chat-app/chat-backend/router/user.js
+++ chat-app/chat-backend/router/user.js
@@ -3,7 +3,8 @@ const { update } = require("../controller/userController");
 const { validate } = require("../validators");
 const { auth } = require("../middleware/auth");
 const { rules: updadeRules } = require("../validators/user/update");
+const { userFile } = require("../middleware/fileUpload");
 
-router.post("/update", [auth, updadeRules, validate], update);
+router.post("/update", [auth, userFile, updadeRules, validate], update);
 
 module.exports = router;
