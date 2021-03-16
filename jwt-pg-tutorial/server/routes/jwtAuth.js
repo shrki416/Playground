e5451223d128045c4e45eb085af19873625b75cf
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2021-03-15 21:33:48 -0400

    routes directory

---
+++ jwt-pg-tutorial/server/routes/jwtAuth.js
@@ -0,0 +1,45 @@
+const router = require("express").Router();
+const pool = require("../database/db");
+const bcrypt = require("bcrypt");
+
+//Register Route
+router.post("/register", async (req, res) => {
+  try {
+    // 1. destructure req.body (name, email, password)
+    // 2. check if user exist (if user exist then throw error)
+    // 3. bcrypt the user password
+    // 4. enter the new user inside our database
+    // 5. generating out jwt token
+
+    // 1. destructure req.body (name, email, password)
+    const { name, email, password } = req.body;
+
+    // 2. check if user exist (if user exist then throw error)
+    const user = await pool.query("SELECT * FROM users WHERE user_email = $1", [
+      email,
+    ]);
+
+    if (user.rows.length !== 0) {
+      return res.status(401).send("User already exists");
+    }
+
+    // 3. bcrypt the user password
+    const saltRounds = 10;
+    const salt = await bcrypt.genSalt(saltRounds);
+
+    const bcryptPassword = await bcrypt.hash(password, salt);
+
+    // 4. enter the new user inside our database
+    let newUser = await pool.query(
+      "INSERT INTO users (user_name, user_email, user_password) VALUES ($1, $2, $3) RETURNING *",
+      [name, email, bcryptPassword]
+    );
+
+    res.json(newUser.rows[0]);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+module.exports = router;
