113171dd5d04c197bfbb232933d6153e0d03a915
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2021-03-16 21:22:52 -0400

    Register Route: WIP

--- jwt-pg-tutorial/client/src/components/Register.js
+++ jwt-pg-tutorial/client/src/components/Register.js
@@ -1,10 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
 
-function Register() {
+function Register({ setAuth }) {
+  const [input, setInput] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const { name, email, password } = input;
+  const onChange = (e) => {
+    setInput({ ...input, [e.target.name]: e.target.value });
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const body = { email, password, name };
+      const response = await fetch("http://localhost:5000/auth/register", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body),
+      });
+
+      const parseResponse = await response.json();
+      localStorage.setItem("token", parseResponse.token);
+      setAuth(true);
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
   return (
-    <div>
-      <h1>Register Page</h1>
-    </div>
+    <>
+      <h1>Register</h1>
+      <form className="container d-grid" onSubmit={onSubmit}>
+        <input
+          className="form-control my-3"
+          type="name"
+          name="name"
+          placeholder="name"
+          value={name}
+          onChange={(e) => onChange(e)}
+        />
+        <input
+          className="form-control my-3"
+          type="email"
+          name="email"
+          placeholder="email"
+          value={email}
+          onChange={(e) => onChange(e)}
+        />
+        <input
+          className="form-control my-3"
+          type="password"
+          name="password"
+          placeholder="password"
+          value={password}
+          onChange={(e) => onChange(e)}
+        />
+        <button className="btn btn-success" type="submit">
+          Submit
+        </button>
+      </form>
+    </>
   );
 }
 
