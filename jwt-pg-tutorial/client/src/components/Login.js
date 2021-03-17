6b9878cc6b6d4cbc80c3ff4fb7f2d238c5de689c
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2021-03-17 19:27:33 -0400

    Login Component: WIP

--- jwt-pg-tutorial/client/src/components/Login.js
+++ jwt-pg-tutorial/client/src/components/Login.js
@@ -1,6 +1,17 @@
 import React, { useState } from "react";
 
 function Login({ setAuth }) {
+  const [inputs, setInputs] = useState({
+    email: "",
+    password: "",
+  });
+
+  const { email, password } = inputs;
+
+  const onChange = (e) => {
+    setInputs({ ...inputs, [e.target.name]: e.target.value });
+  };
+
   return (
     <>
       <h1>Login</h1>
@@ -9,6 +20,8 @@ function Login({ setAuth }) {
           className="form-control my-3"
           type="email"
           name="email"
+          value={email}
+          onChange={(e) => onChange(e)}
           placeholder="email"
         />
         <input
@@ -16,6 +29,8 @@ function Login({ setAuth }) {
           type="password"
           name="password"
           placeholder="password"
+          value={password}
+          onChange={(e) => onChange(e)}
         />
         <button className="btn btn-primary btn-block">Login</button>
       </form>
