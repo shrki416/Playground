d02de0417d394021c44044d7455e6f9730905433
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2021-03-16 21:34:24 -0400

    Login: WIP

--- jwt-pg-tutorial/client/src/components/Login.js
+++ jwt-pg-tutorial/client/src/components/Login.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
-function Login() {
+function Login({ setAuth }) {
   return (
-    <div>
-      <h1>Login Page</h1>
-    </div>
+    <>
+      <h1>Login</h1>
+      <form className="text-center container d-grid my-5">
+        <input
+          className="form-control my-3"
+          type="email"
+          name="email"
+          placeholder="email"
+        />
+        <input
+          className="form-control my-3"
+          type="password"
+          name="password"
+          placeholder="password"
+        />
+        <button className="btn btn-primary btn-block">Login</button>
+      </form>
+    </>
   );
 }
 
