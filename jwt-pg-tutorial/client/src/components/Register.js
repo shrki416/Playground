4b743a6659998e50d61bbde8aba92529afba6a2f
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2021-03-18 09:38:50 -0400

    register complete

--- jwt-pg-tutorial/client/src/components/Register.js
+++ jwt-pg-tutorial/client/src/components/Register.js
@@ -16,7 +16,7 @@ function Register({ setAuth }) {
   const onSubmit = async (e) => {
     e.preventDefault();
     try {
-      const body = { email, password, name };
+      const body = { name, email, password };
       const response = await fetch("http://localhost:5000/auth/register", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
