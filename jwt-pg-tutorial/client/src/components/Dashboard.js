eb30ae63ba14fc9b151fd72d2999de25632241ef
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2021-03-18 09:38:10 -0400

    Logout: WIP

--- jwt-pg-tutorial/client/src/components/Dashboard.js
+++ jwt-pg-tutorial/client/src/components/Dashboard.js
@@ -1,10 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-function Dashboard() {
+function Dashboard({ setAuth }) {
+  const [name, setName] = useState();
+
+  async function getProfile() {
+    try {
+      const response = await fetch("http://localhost:5000/dashboard", {
+        method: "GET",
+        headers: { token: localStorage.token },
+      });
+
+      const parseResponse = await response.json();
+      setName(parseResponse.user_name);
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+
+  const logout = (e) => {
+    e.preventDefault();
+    try {
+      localStorage.removeItem("token");
+      setAuth(false);
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
+  useEffect(() => {
+    getProfile();
+  });
+
   return (
     <div>
       <h1>Dashboard Page</h1>
-      <button className="btn btn-success">LOGOUT</button>
+      <div>Welcome {name}</div>
+      <button onClick={(e) => logout(e)} className="btn btn-success">
+        LOGOUT
+      </button>
     </div>
   );
 }
