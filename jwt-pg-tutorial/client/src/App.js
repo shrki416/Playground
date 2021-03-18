b4fe4e1c2837d958ac71fa42e9b43aa2f1c5bbdc
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2021-03-18 09:55:06 -0400

    persist user

--- jwt-pg-tutorial/client/src/App.js
+++ jwt-pg-tutorial/client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   BrowserRouter as Router,
   Switch,
@@ -16,6 +16,25 @@ function App() {
   const setAuth = (boolean) => {
     setIsAuthenticated(boolean);
   };
+
+  async function isAuth() {
+    try {
+      const response = await fetch("http://localhost:5000/auth/verify", {
+        method: "GET",
+        headers: { token: localStorage.token },
+      });
+      const parseResponse = await response.json();
+      parseResponse === true
+        ? setIsAuthenticated(true)
+        : setIsAuthenticated(false);
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+
+  useEffect(() => {
+    isAuth();
+  }, []);
 
   return (
     <div className="App">
