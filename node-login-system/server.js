2df178df7ac601c7d8ebac4f90b0044d9124ec62
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2021-04-23 20:18:47 -0400

    Basic server set-up

---
+++ node-login-system/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.set("view-engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.listen(PORT, () => `live on ${PORT} ⚜️`);
